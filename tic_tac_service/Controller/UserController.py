00181440bf2d709c6c5a59a56bb663b551c05607
JustJaw <justin.williams.2@stonybrook.edu>
2018-02-20 23:55:46 -0500

    Testing email sending

--- tic_tac_service/Controller/UserController.py
+++ tic_tac_service/Controller/UserController.py
@@ -1,7 +1,23 @@
 import falcon
-import pymongo
+from pymongo import MongoClient
 import smtplib
 
+KEY = "abracadabra"
+FROM = 'JustinandTed@python.com'
+SUBJECT = "Hello!"
+TEXT = "This message was sent with Python's smtplib."
+
+def sendMail(user):
+
+    toUser = [user['email']]  # must be a list
+
+    # Prepare actual message
+    message = "This is your key\n\tKEY : " + KEY
+
+    # Send the mail
+    server = smtplib.SMTP('localhost')
+    server.sendmail(FROM, toUser, message)
+    server.quit()
 
 class addUser:
     def on_post(self, req, resp):
@@ -10,78 +26,30 @@ class addUser:
         user['winner'] = " "
         user['grid'] = [" "," "," "," "," "," "," "," "]
 
-
-
-
-
-FROM = 'JustinandTed@python.com'
-
-TO = [user['email']] # must be a list
-
-SUBJECT = "Hello!"
-
-TEXT = "This message was sent with Python's smtplib."
-
-# Prepare actual message
-
-message = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-
-# Send the mail
-
-server = smtplib.SMTP('myserver')
-server.sendmail(FROM, TO, message)
-server.quit()
-
-
-
-
-
-
-
-
-
-
+        sendMail(user)
 
         print(user)
         resp.media = user
 
-
-
-
 class verifyUser:
+    def on_post(self, req, resp):
+        user = req.media
+        userEmail= user['email']
 
+        client = MongoClient()
+        db = client['tic-tac-toe']
 
-def on_post(self, req, resp):
-user = req.media
-userEmail= user['email']
+        collection= db['users']
 
-client= MongoClient()
-db = client['tic-tac-toe']
+        userFromDB= db.collection.find_one({'email': userEmail})
 
-collection= db['users']
+        user_enabled = {"enabled": True}
 
-from bson.bject import ObjectId
-
-userFromDB= db.collection.find_one({'email': userEmail})
-
-if(userFromDB=""){
-	
-	print("Wrong email")
-}
-
-
-else{
-userFromDB['enabled'] = True
-	
-
-collection.update_one({'_id':userFromDB['_id']}, {"$set": post}, upsert=False)
-
-
-resp.media =user
-}
+        if userFromDB == "":    
+            print("Wrong email")
+        else:
+            userFromDB['enabled'] = True
+            collection.update_one({'_id': userFromDB['_id']}, {
+                                  "$set": user_enabled}, upsert=False)
+            resp.media =user
+    
--- tic_tac_service/app.py
+++ tic_tac_service/app.py
@@ -2,6 +2,7 @@ import falcon
 import routes
 
 
+
 api = falcon.API()
 api = routes.addRoutes(api)
     

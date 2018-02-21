9f1db7c22446f47a58ad7b2e033ad0342bbe43ec
Unknown <tachoudhury@cs.stonybrook.edu>
2018-02-20 23:20:09 -0500

    Auto stash before merge of "master" and "origin/master"
    added to usercontroller

--- tic_tac_service/Controller/UserController.py
+++ tic_tac_service/Controller/UserController.py
@@ -1,10 +1,52 @@
 import falcon
 import pymongo
+import smtplib
+
 
 class addUser:
     def on_post(self, req, resp):
         user = req.media
         user['enabled'] = False
+        user['winner'] = " "
+        user['grid'] = [" "," "," "," "," "," "," "," "]
+
+
+
+
+
+FROM = 'JustinandTed@python.com'
+
+TO = [user['email']] # must be a list
+
+SUBJECT = "Hello!"
+
+TEXT = "This message was sent with Python's smtplib."
+
+# Prepare actual message
+
+message = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+# Send the mail
+
+server = smtplib.SMTP('myserver')
+server.sendmail(FROM, TO, message)
+server.quit()
+
+
+
+
+
+
+
+
+
+
 
         print(user)
         resp.media = user
@@ -12,3 +54,34 @@ class addUser:
 
 
 
+class verifyUser:
+
+
+def on_post(self, req, resp):
+user = req.media
+userEmail= user['email']
+
+client= MongoClient()
+db = client['tic-tac-toe']
+
+collection= db['users']
+
+from bson.bject import ObjectId
+
+userFromDB= db.collection.find_one({'email': userEmail})
+
+if(userFromDB=""){
+	
+	print("Wrong email")
+}
+
+
+else{
+userFromDB['enabled'] = True
+	
+
+collection.update_one({'_id':userFromDB['_id']}, {"$set": post}, upsert=False)
+
+
+resp.media =user
+}
--- tic_tac_service/routes.py
+++ tic_tac_service/routes.py
@@ -5,4 +5,5 @@ import Controller.UserController as UserResource
 def addRoutes(api):
     api.add_route('/ttt/play', game.PlayResource())
     api.add_route('/adduser', UserResource.addUser())
+    api.add_route('/verify', UserResource.verifyUser())
     return api

206d863eef1af685a5827b4de18e0fd6b5922dfd
JustJaw <justin.williams.2@stonybrook.edu>
2018-02-21 01:55:33 -0500

    Email sending works to a degree

--- tic_tac_service/Controller/MailController.py
+++ tic_tac_service/Controller/MailController.py
@@ -1,5 +1,4 @@
 import smtplib
-import os
 
 SERVER = 'localhost'
 
@@ -7,15 +6,7 @@ def sendMail(to_email,email_content):
     email_from = "Tic-Tac@Tac.com"
     email_subject = 'Verify Email'
 
-    # Prepare actual message
-    message = """\
-    From: %s
-    To: %s
-    Subject: %s
-
-    %s
-    """ % (email_from, ", ".join(to_email), email_subject, email_content)
-
     smtp = smtplib.SMTP(SERVER)
-    smtp.sendmail(email_from, to_email, message)
+    smtp.starttls()
+    smtp.sendmail(email_from, to_email, email_content)
     smtp.quit()
